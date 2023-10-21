8edefee641b12290725b1730ffdf995ece32ee3d
degiminnal <degim@tju.edu.cn>
2023-10-22 03:49:04 +0800

    10

--- app/app.py
+++ app/app.py
@@ -2,41 +2,42 @@ import os
 import numpy as np
 import pandas as pd
 from joblib import load
-from flask import Flask, render_template, request
+from sklearn.tree import DecisionTreeClassifier
+from flask import Flask, render_template, request, current_app
 
-app = Flask(__name__)
-with app.app_context():
-    import numpy as np
-    from sklearn.tree import DecisionTreeClassifier
-    class MyClassfier(DecisionTreeClassifier):
-        def __init__(self):
-            super().__init__()
-            self.dic = dict()
-            
-        def encodeStr(self, x):
-            x = x.copy()
-            for i,col in enumerate(x.columns):
-                if str(x[col].dtype)!="object":
-                    continue
-                dic = {_:i for i,_ in enumerate(list(set(x[col])))}
-                x[col] = np.array([dic[_] for _ in x[col]])
-                self.dic[i] = dic
-            return x
+class MyClassfier(DecisionTreeClassifier):
+    def __init__(self):
+        super().__init__()
+        self.dic = dict()
         
-        def fit(self, x, y):
-            x = self.encodeStr(x)
-            return super().fit(x,y)
-            
-        def convert_(self,x):
-            try:
-                float(x)
-                return float(x)
-            except:
-                return -1
+    def encodeStr(self, x):
+        x = x.copy()
+        for i,col in enumerate(x.columns):
+            if str(x[col].dtype)!="object":
+                continue
+            dic = {_:i for i,_ in enumerate(list(set(x[col])))}
+            x[col] = np.array([dic[_] for _ in x[col]])
+            self.dic[i] = dic
+        return x
+    
+    def fit(self, x, y):
+        x = self.encodeStr(x)
+        return super().fit(x,y)
+        
+    def convert_(self,x):
+        try:
+            float(x)
+            return float(x)
+        except:
+            return -1
 
-        def predict(self, datas):
-            datas = [[self.dic.get(i_,{x_:self.convert_(x_)}).get(x_, -1) for i_,x_ in enumerate(data)] for data in datas]
-            return super().predict(datas)
+    def predict(self, datas):
+        datas = [[self.dic.get(i_,{x_:self.convert_(x_)}).get(x_, -1) for i_,x_ in enumerate(data)] for data in datas]
+        return super().predict(datas)
+
+
+
+app = current_app
 
 @app.route('/', methods=['GET', 'POST'])
 def route_index():

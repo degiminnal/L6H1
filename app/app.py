d00c8fc3d8da7e3a83bb0cffe823389f8b523273
degiminnal <degim@tju.edu.cn>
2023-10-22 06:22:52 +0800

    11

--- app/app.py
+++ app/app.py
@@ -5,42 +5,10 @@ import pandas as pd
 from joblib import load
 from sklearn.tree import DecisionTreeClassifier
 from flask import Flask, render_template, request
-
-class MyClassfier(DecisionTreeClassifier):
-    def __init__(self):
-        super().__init__()
-        self.dic = dict()
-        
-    def encodeStr(self, x):
-        x = x.copy()
-        for i,col in enumerate(x.columns):
-            if str(x[col].dtype)!="object":
-                continue
-            dic = {_:i for i,_ in enumerate(list(set(x[col])))}
-            x[col] = np.array([dic[_] for _ in x[col]])
-            self.dic[i] = dic
-        return x
-    
-    def fit(self, x, y):
-        x = self.encodeStr(x)
-        return super().fit(x,y)
-        
-    def convert_(self,x):
-        try:
-            float(x)
-            return float(x)
-        except:
-            return -1
-
-    def predict(self, datas):
-        datas = [[self.dic.get(i_,{x_:self.convert_(x_)}).get(x_, -1) for i_,x_ in enumerate(data)] for data in datas]
-        return super().predict(datas)
-
+from model import MyClassfier
 
 
 app = Flask(__name__)
-
-app.url_map.converters['clf'] = MyClassfier
 
 @app.route('/', methods=['GET', 'POST'])
 def route_index():
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from app.app import app
+from app.model import MyClassfier
 
 if __name__ == "__main__":
 	app.run()
